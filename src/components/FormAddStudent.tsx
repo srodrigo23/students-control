ab5db1db5dd0a054889edf17be130620f43ddf3b
rodres101 <rodrigosergio93@gmail.com>
2023-08-13 18:38:15 -0400

    add student v1

---
+++ src/components/FormAddStudent.tsx
@@ -0,0 +1,65 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+
+type Inputs = {
+    name: string,
+    lastname: string,
+    codsis: number,
+    ci : number,
+    // exampleRequired: string,
+  };
+
+const FormAddStudent = ()=>{
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+    return(
+        <>
+            {/* /* "handleSubmit" will validate your inputs before invoking "onSubmit" */ }
+            <div
+                className="rounded boder-solid border-indigo-600 border-2 m-5"
+            >
+                
+                <form 
+                    className="grid grid-flow-column-dense grid-cols-3 p-5"
+                    // onSubmit={handleSubmit(onSubmit)}
+                >                
+                    <a className="col-span-1">Nombres :</a>
+                    <input 
+                        className="col-span-2 rounded border-solid border-2"
+                        defaultValue="" {...register("name", { required: true })} />
+                    { errors.name && <span>This field is required</span> }
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <br/>
+                    <a className="col-span-1">Apellidos :</a>
+                    <input 
+                        className="col-span-2 rounded border-solid border-2"
+                        defaultValue="" {...register("lastname", { required: true })} />
+                    { errors.name && <span>This field is required</span> }
+                    <br/>
+                    
+                    <a className="col-span-1">Cod. SIS :</a>
+                    <input 
+                        className="rounded border-solid border-2"
+                        defaultValue="" {...register("codsis")} />
+                    <br/>
+                    <a className="col-span-1">Ci :</a>
+                    <input 
+                        className="rounded border-solid border-2"
+                        defaultValue="" {...register("ci")} />
+                    <br/>
+
+                    <button
+                        className="bg-indigo-500 border-white border-solid rounded-md text-white mt-5 pt-2 pb-2 pl-4 pr-4"
+                        type="submit">
+                            Crear
+                    </button>
+                
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default FormAddStudent;
---
+++ src/pages/addStudent.tsx
@@ -0,0 +1,20 @@
+import {type NextPage} from "next"
+import FormAddStudent from "~/components/FormAddStudent";
+
+
+
+const AddStudent: NextPage = () =>{
+    return(
+        <> 
+            <h1 className="border p-3 m-3">
+                Add Students:
+            </h1>
+           <FormAddStudent
+                
+           ></FormAddStudent>
+        </>
+    )
+
+}
+
+export default AddStudent;
